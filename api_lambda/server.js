44126478b6704705193d16cfd7a40a852373f9e6
Robert <bibbycodes@gmail.com>
2020-03-27 11:08:07 +0000

    api - rates/source/target - symbols/exch/sym'

--- api_lambda/server.js
+++ api_lambda/server.js
@@ -7,11 +7,47 @@ const dbConn = require('./dbConn')
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
-app.get('/rates', async (req, res) => {
+app.get('/rates/all', async (req, res) => {
   let db = new dbConn()
   let result = await db.query('SELECT * FROM tw_exchange_rates_history;').catch(err => console.log(err))
   let rates = result.rows
   res.json(rates)
+})
+
+app.get('/rates/last/:source/:target', async (req, res) => {
+  let db = new dbConn()
+  let source = req.params.source.toUpperCase()
+  let target = req.params.target.toUpperCase()
+  let result = await db.query(
+    `SELECT * FROM tw_exchange_rates_history
+    WHERE source='${source}' AND target='${target}';`
+    ).catch(err => console.log(err))
+  let rates = result.rows
+  console.log(result.rows)
+  res.json(rates)
+})
+
+
+
+app.get('/symbols/all', async(req, res) => {
+  let db = new dbConn()
+  let result = await db.query('SELECT * FROM order_book_best;')
+  res.json(result.rows)
+})
+
+app.get('/symbols/:exchange/:symbol/', async(req, res) => {
+  let db = new dbConn()
+  let exchange = req.params.exchange
+  let symbol = req.params.symbol
+  let result = await db.query(
+    `SELECT * FROM order_book_best
+    WHERE exchange='${exchange}' AND pair='${symbol}';`
+    )
+  res.send(result.rows)
+})
+
+app.get('*', (req, res) => {
+  res.send('CryptoArb')
 })
 
 app.listen(PORT, () => {
