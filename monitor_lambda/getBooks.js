b0f1bbd91a7b98c85d37302d7c647a49af6b4cb5
Robert <bibbycodes@gmail.com>
2020-03-28 17:55:59 +0000

    added more pairs

--- monitor_lambda/getBooks.js
+++ monitor_lambda/getBooks.js
@@ -1,17 +1,17 @@
 exports.func = async () => {
   const MonitorRest = require('./models/MonitorRest')
   const Generate = require('./models/Generate')
-  let cryptos = ['BTC', 'ETH', 'LTC',]
-  let currencires = ['GBP', 'NGN', 'EUR', 'USD']
+  let cryptos = ['BTC', 'ETH', 'LTC', 'USDT', 'USDC', 'BGBP', 'XRP', 'NEO']
+  let currencires = ['GBP', 'NGN', 'EUR', 'USD', 'USDC', 'ETH', 'USDT', 'LTC', 'BGBP']
   let symbols = Generate.pairs(cryptos, currencires)
-  let exchanges = ['binance', 'kraken', 'coinbasepro', 'bittrex', 'poloniex']
+  let exchanges = ['binance', 'kraken', 'coinbasepro', 'bittrex', 'poloniex', 'gemini']
 
   for (let exchange of exchanges) {
     for (let symbol of symbols) {
       if (exchange == 'binance' || exchange == 'poloniex') {
         let symbol_split = symbol.split('/')
         if (symbol_split[1] == 'USD') {
-          symbol = symbol_split[0] + '/USDT'
+          continue
         }
       }
       await MonitorRest.orderBook(exchange, symbol)
