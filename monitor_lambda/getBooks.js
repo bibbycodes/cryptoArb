ab7c8434ff0e850230aba4e33e885cb10ec1d481
Temitope Alabi <alabitemitope@gmail.com>
2020-04-01 01:14:53 -0500

    Wip

--- monitor_lambda/getBooks.js
+++ monitor_lambda/getBooks.js
@@ -19,25 +19,28 @@
 //   }
 // } 
 
-async function mon() {
-  const MonitorRest = require('./models/MonitorRest')
-  const Generate = require('./models/Generate')
-  let cryptos = ['BTC', 'ETH', 'LTC', 'USDT', 'USDC', 'BGBP', 'XRP', 'NEO']
-  let currencires = ['GBP', 'NGN', 'EUR', 'USD', 'USDC', 'ETH', 'USDT', 'LTC', 'BGBP']
-  let symbols = Generate.pairs(cryptos, currencires)
-  let exchanges = ['bittrex', 'poloniex', 'gemini']
 
-  for (let exchange of exchanges) {
-    for (let symbol of symbols) {
-      if (exchange == 'binance' || exchange == 'poloniex') {
-        let symbol_split = symbol.split('/')
-        if (symbol_split[1] == 'USD') {
-          continue
+
+exports.func = async () => {
+  async function mon() {
+    const MonitorRest = require('./models/MonitorRest')
+    const Generate = require('./models/Generate')
+    let cryptos = ['BTC', 'ETH', 'LTC', 'USDT', 'USDC', 'BGBP', 'XRP', 'NEO']
+    let currencires = ['GBP', 'NGN', 'EUR', 'USD', 'USDC', 'ETH', 'USDT', 'LTC', 'BGBP']
+    let symbols = Generate.pairs(cryptos, currencires)
+    let exchanges = ['bittrex', 'poloniex', 'gemini']
+
+    for (let exchange of exchanges) {
+      for (let symbol of symbols) {
+        if (exchange == 'binance' || exchange == 'poloniex') {
+          let symbol_split = symbol.split('/')
+          if (symbol_split[1] == 'USD') {
+            continue
+          }
         }
+        await MonitorRest.orderBook(exchange, symbol)
       }
-      await MonitorRest.orderBook(exchange, symbol)
     }
   }
 }
-
 mon()
